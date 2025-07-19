d9ebbf87de41d66b33f87ec1c86e67e48ab6232c
Sanchit Ghag <ghagsanchit0@gmail.com>
2025-07-19 22:27:20 +0530

    Install and setup the config

--- Taskmanagement/settings.py
+++ Taskmanagement/settings.py
@@ -1,6 +1,7 @@
 
 from pathlib import Path
 import os
+from decouple  import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -132,16 +133,17 @@ MEDIA_ROOT =  os.path.join(BASE_DIR  , 'media')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-# AUTH_USER_MODEL = 'dashboard.User'
 
 
 ## email here
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "kislayraj69@gmail.com"
-EMAIL_HOST_PASSWORD = "pyvdyzqkzrztgysi"
+
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
+
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
